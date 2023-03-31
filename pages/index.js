aee9ed99607ab2c35efecef5e0135f38d7035366
Seowoo <seosydneyweb@gmail.com>
2023-04-01 08:52:23 +1100

    update

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
 import Head from 'next/head'
-// import Image from 'next/image'
 import { Inter } from 'next/font/google'
 import styles from '@/styles/Home.module.css'
 import { GraphQLClient, gql } from 'graphql-request';
 import BlogCard from '../components/BlogCard';
-// import Datetime from 'react-datetime';
 
 const inter = Inter({ subsets: ['latin'] })
 
